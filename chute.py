1b487925bf42ff91531ca8691f5c389bba5b8b42
Nícolas Augusto Grande <89585453+NicolasAGrande@users.noreply.github.com>
2022-02-24 07:51:53 -0300

    Update chute.py

--- chute.py
+++ chute.py
@@ -5,7 +5,7 @@ num = randrange(1, 7)
 x = 0
 while x != num:
     x = int(input('Adivinhe o valor que estou pensando(entre 1 e 6): '))
-    if x > 6 or x < 6:
+    if x > 6 or x < 0:
         print('Digite um valor entre 1 e 6')
 
 print('Você conseguiu!')
